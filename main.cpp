94b4920469010c9e7ab81c6c267adbd0a7cbfaf0
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-12-10 20:13:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    // punto 1: definisco la dimensione dell'array
+    int a=0;
+    float num=0;
+    cin >> a;
+    if (a<=0){
+        return 0;
+    }
+// punto 3: rende da tastiera i valori da salvare all’interno dell’array
+float numeri[a];
+    for (int i = 0; i<a; i++){
+        num=0;
+        cin >> num;
+        numeri [i] = num;
+    }
+cout << num_maggiore(numeri, a)<< endl;
+    return 0;
 }
