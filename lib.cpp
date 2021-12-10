4300971d57ada5601767a707a8784a996985edc8
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-12-10 20:11:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+using namespace std;
+//Svolgimento richiesta principale: trovare il numero maggiore inserito
+float num_maggiore (float numeri[], int dim){
+    float b=0;
+    b=numeri [0];
+    for (int j=0; j<dim; j++){
+        if (b<numeri [j]){
+            b=numeri [j];
+        }
+    }
+    return b;
+}
